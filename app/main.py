eca536826f1e7d16001a9892c9d916ff2aa9e1fc
Teashrock <kajitsu22@gmail.com>
2026-05-24 17:47:21 +0300

    Ensured kwargs are used as a key

--- app/main.py
+++ app/main.py
@@ -10,10 +10,10 @@ def cache(func: Callable) -> Callable:
         if cache_dict.get(func.__name__, None) is None:
             cache_dict[func.__name__] = {}
         curr_dict = cache_dict[func.__name__]
-        if curr_dict.get(tuple(args), None) is None:
+        if curr_dict.get(tuple(args) + tuple(kwargs), None) is None:
             print("Calculating new result")
-            curr_dict[tuple(args)] = func(*args, **kwargs)
+            curr_dict[tuple(args) + tuple(kwargs)] = func(*args, **kwargs)
         else:
             print("Getting from cache")
-        return curr_dict[tuple(args)]
+        return curr_dict[tuple(args) + tuple(kwargs)]
     return wrapper
